e8545150f7ea6e33bcde7a66a50325f76c6f0f3a
“xiaobq” <xiaobq@126.com>
2017-08-15 07:17:21 +0800

    1st commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const numbers = [1, 2, 3, 4, 5,6];
+const listItems = numbers.map((number) =>
+  <li>{number}</li>
+);
+ReactDOM.render(
+  <ul>{listItems}</ul>,
+  document.getElementById('root')
+);
